ba3e8a66dbb71458ee3d129d72a4049180f612d4
Phathy <admin@phathy.com>
2025-01-04 17:51:57 +0700

    get data from uv api based on the latitude and longtitude

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ import axios from "axios";
 const app = express();
 import { readFileSync } from "fs"; //after finished reading the file then execute the next line
 const port = 3000;
-const apiURL = "https://api.openuv.io/api/v1/uv";
+const apiURL = "https://api.openuv.io/api/v1/uv/";
+const config = {
+  headers: {
+    "x-access-token": "openuv-24ehrm5gi59ab-io",
+  },
+};
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
 
 const data = JSON.parse(readFileSync("./data/countries+states.json", "utf8")); //json content in javascript object syntax
 
@@ -22,14 +26,13 @@ app.post("/", (req, res) => {
 });
 
 app.post("/uv", async (req, res) => {
-  const lat = req.body.cities.latitude;
-  const lon = req.body.longitude;
-  console.log(lat);
-  res.send(req.body.cities)
-  // const response = await axios.get(`${apiURL}?lat=`);
-  // res.redirect("/")
+  const stateData = JSON.parse(req.body.cities);
+  const lat = stateData.latitude;
+  const lon = stateData.longitude;
+  const response = await axios.get(`${apiURL}?lat=${lat}&lng=${lon}`, config);
+  console.log(response.data);
+  res.send(req.body.cities);
 });
-
 
 app.listen(port, () => {
   console.log("Running...");
