daaa4a632c9d5dd4d7dc59525eb5675b2603b27d
Phathy <admin@phathy.com>
2025-01-04 10:34:08 +0700

    add drop down menu to select the countries

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
 import express from "express";
 import axios from "axios";
 const app = express();
-const port = 1212;
-app.use(express.static("public"));
-app.set("view engine", "ejs")
+import { readFileSync } from "fs"; //after finished reading the file then execute the next line
+const port = 3000;
+app.use(express.static("public")); 
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
+
+const data = JSON.parse(readFileSync("./data/countries+states.json", "utf8")); //json content in javascript object syntax
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {data});
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.post("/", (req, res) => {
+  res.send(req.body.country);
+})
 
 app.listen(port, () => {
   console.log("Running...");
+
 });
